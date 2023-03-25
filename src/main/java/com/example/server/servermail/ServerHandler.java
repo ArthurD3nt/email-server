64b281bb846c22e53cee582fb780f3b95065c121
AlessandroMilani <alessandromilani2000@gmail.com>
2023-03-25 18:11:26 +0100

    controllo sender e reciver per aggiornamento email

--- src/main/java/com/example/server/servermail/ServerHandler.java
+++ src/main/java/com/example/server/servermail/ServerHandler.java
@@ -145,8 +145,8 @@ public class ServerHandler implements Runnable{
 
         ArrayList<EmailBody> emailBodies = new ArrayList<>();
         for(EmailBody e: user.getEmails()){
-            if(e.getTimestamp().after(getEmailsBody.getTimestamp())) {
-              emailBodies.add(e);
+            if(e.getTimestamp().after(getEmailsBody.getTimestamp()) && (!getEmailsBody.getEmail().equals(e.getSender())) || e.getSender().equals(e.getReceivers().get(0))) {
+                emailBodies.add(e);
             }
         }
 
