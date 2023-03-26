bb63f85e87152177fe9c29ddbfc48947cba0d82c
ArthurD3nt <francescomazzucco01@gmail.com>
2023-03-26 10:54:07 +0200

    Spostato inizializzazione di input e output stream nel constructor di ClientMain e fatto handling delle exceptions

--- src/main/java/com/example/server/model/UserService.java
+++ src/main/java/com/example/server/model/UserService.java
@@ -15,90 +15,97 @@ public class UserService {
     private static UserService instance = null;
 
     private LogModel logModel;
+    ObjectOutputStream out;
 
-    private static HashMap<String,ReentrantReadWriteLock > filesLock = new HashMap<>();
+    private static HashMap<String, ReentrantReadWriteLock> filesLock = new HashMap<>();
 
     private final static String FILE_PATH_MAC = "/Users/matteomarengo/Documents/uni/email-server/src/main/java/com/example/server/email/";
     private final static String FILE_PATH_MINT = "/home/ale/github/email-server/src/main/java/com/example/email";
     private final static String FILE_PATH_WIN = "/home/frama/git/email-server/src/main/java/com/example/server/email/";
 
-    private String FILE_PATH_TO_USE = FILE_PATH_MAC;
-    private UserService(LogModel logModel) {
+    private String FILE_PATH_TO_USE = FILE_PATH_WIN;
+
+    private UserService(LogModel logModel, ObjectOutputStream out) {
         this.logModel = logModel;
+        this.out = out;
     }
 
-    public static synchronized UserService getInstance(LogModel logModel){
-        if(instance == null)
-            instance = new UserService(logModel);
+    public static synchronized UserService getInstance(LogModel logModel, ObjectOutputStream out) {
+        if (instance == null)
+            instance = new UserService(logModel, out);
         return instance;
     }
 
 
-    public User createUser(String email){
+    public User createUser(String email) {
         blockWrite(email);
-        User u = new User(email,new ArrayList<>());
+        User u = new User(email, new ArrayList<>());
         writeUserToFile(u);
         return u;
     }
 
-    public User readUserFromFile(String email) throws IOException{
+    public User readUserFromFile(String email) throws IOException {
         Lock read = blockRead(email);
         read.lock();
-        
-        try(ObjectInputStream objectInput = new ObjectInputStream((new FileInputStream(FILE_PATH_TO_USE+email.toLowerCase()+".txt")))){
-            return (User)objectInput.readObject();
-        } catch ( IOException | ClassNotFoundException fileNotFoundException) {
-            logModel.setLog("ERROR: file" + FILE_PATH_TO_USE+email.toLowerCase() + "not found");
-        }finally {
+
+        try (ObjectInputStream objectInput = new ObjectInputStream((new FileInputStream(FILE_PATH_TO_USE + email.toLowerCase() + ".txt")))) {
+            return (User) objectInput.readObject();
+        } catch (IOException | ClassNotFoundException fileNotFoundException) {
+            logModel.setLog("ERROR: file" + FILE_PATH_TO_USE + email.toLowerCase() + "not found");
+            out.writeObject(new Communication("server_error", new ErrorBody("fileNotFoundException")));
+        } finally {
             read.unlock();
         }
         return null;
     }
 
-    public User readUserFromFileBlocking(String email){
+    public User readUserFromFileBlocking(String email) throws IOException {
         blockWrite(email);
-        try(ObjectInputStream objectInput = new ObjectInputStream((new FileInputStream(FILE_PATH_TO_USE+email.toLowerCase()+".txt")))){
-            return (User)objectInput.readObject();
-        } catch ( IOException | ClassNotFoundException fileNotFoundException) {
-            logModel.setLog("ERROR: file " + FILE_PATH_TO_USE+email.toLowerCase() + ".txt not found");
+        try (ObjectInputStream objectInput = new ObjectInputStream((new FileInputStream(FILE_PATH_TO_USE + email.toLowerCase() + ".txt")))) {
+            return (User) objectInput.readObject();
+        } catch (IOException | ClassNotFoundException fileNotFoundException) {
+            logModel.setLog("ERROR: file " + FILE_PATH_TO_USE + email.toLowerCase() + ".txt not found");
+            out.writeObject(new Communication("server_error", new ErrorBody("fileNotFoundException")));
         }
         return null;
     }
 
-    private synchronized Lock blockRead(String email){
+    private synchronized Lock blockRead(String email) {
         Lock read;
         /**
          * Controlla che sia presente il lock associato all'email:
          * se presente: associa a read il lock e lo restituisce
          * se non presente crea un lock, lo inserisce nella mappa filesLock e associa a read il lock e lo restituisce
          * */
-        if(filesLock.containsKey(email)){
+        if (filesLock.containsKey(email)) {
             read = filesLock.get(email).readLock();
-        }else{
+        } else {
             ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
-            filesLock.put(email,lock);
+            filesLock.put(email, lock);
             read = lock.readLock();
         }
         return read;
     }
-    private synchronized void blockWrite(String email){
+
+    private synchronized void blockWrite(String email) {
         Lock write;
-        if(filesLock.containsKey(email)){
+        if (filesLock.containsKey(email)) {
             write = filesLock.get(email).writeLock();
-        }else{
+        } else {
             ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
-            filesLock.put(email,lock);
+            filesLock.put(email, lock);
             write = lock.writeLock();
         }
         write.lock();
     }
-    public synchronized void unlock(String email){
+
+    public synchronized void unlock(String email) {
         Lock write;
-        if(filesLock.containsKey(email)){
+        if (filesLock.containsKey(email)) {
             write = filesLock.get(email).writeLock();
-        }else{
+        } else {
             ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
-            filesLock.put(email,lock);
+            filesLock.put(email, lock);
             write = lock.writeLock();
         }
         write.unlock();
@@ -113,7 +120,7 @@ public class UserService {
             objectOut.writeObject(user);
         } catch (Exception ex) {
             ex.printStackTrace();
-        }finally {
+        } finally {
             unlock(user.getUser());
         }
 
--- src/main/java/com/example/server/servermail/ServerHandler.java
+++ src/main/java/com/example/server/servermail/ServerHandler.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 
-public class ServerHandler implements Runnable{
+public class ServerHandler implements Runnable {
 
     private Socket socket;
     ObjectInputStream in;
@@ -23,14 +23,31 @@ public class ServerHandler implements Runnable{
         this.logModel = logModel;
         this.socket = socket;
         this.counter = counter;
-        this.userService = UserService.getInstance(logModel);
+
+        try {
+//            try {
+                InputStream inStream = socket.getInputStream();
+                in = new ObjectInputStream(inStream);
+                OutputStream outStream = socket.getOutputStream();
+                out = new ObjectOutputStream(outStream);
+//            } finally {
+//                System.out.println("FINITO");
+//                logModel.setLog("Client disconnected");
+//                socket.close();
+//            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        this.userService = UserService.getInstance(logModel, out);
     }
 
     private void connection(String email) throws IOException {
         logModel.setLog("Client " + email + " connected");
         User user = userService.readUserFromFile(email);
 
-        if(user == null){
+        if (user == null) {
             logModel.setLog("Client " + email + ":null, created!");
             user = userService.createUser(email);
         }
@@ -41,68 +58,63 @@ public class ServerHandler implements Runnable{
         ArrayList<EmailBody> emailBin = new ArrayList<>();
 
         user.getEmails().forEach(emailBody -> {
-           if(emailBody.getReceivers().contains(email) && emailBody.getSender().equals(email) && !emailBody.getBin())
-           {
-               emailReceived.add(emailBody);
-               emailSent.add(emailBody);
-           }
-           else if(emailBody.getReceivers().contains(email) && !emailBody.getBin()){
-               emailReceived.add(emailBody);
-           }
-           else if(emailBody.getSender().equals(email) && !emailBody.getBin()){
-               emailSent.add(emailBody);
-           }
-           else if(emailBody.getBin()){
-               emailBin.add(emailBody);
-           }
+            if (emailBody.getReceivers().contains(email) && emailBody.getSender().equals(email) && !emailBody.getBin()) {
+                emailReceived.add(emailBody);
+                emailSent.add(emailBody);
+            } else if (emailBody.getReceivers().contains(email) && !emailBody.getBin()) {
+                emailReceived.add(emailBody);
+            } else if (emailBody.getSender().equals(email) && !emailBody.getBin()) {
+                emailSent.add(emailBody);
+            } else if (emailBody.getBin()) {
+                emailBin.add(emailBody);
+            }
         });
 
         arrayLists.add(emailReceived);
         arrayLists.add(emailSent);
         arrayLists.add(emailBin);
 
-        Communication response = new Communication("connection_ok", new ConnectionBody(user.getUser(),arrayLists));
+        Communication response = new Communication("connection_ok", new ConnectionBody(user.getUser(), arrayLists));
 
         out.writeObject(response);
     }
 
     private void sendEmail(EmailBody email) throws IOException {
         // blocco il file del sender
-        logModel.setLog("User "+email.getSender()
-                + " sending email: "+email.getId()
+        logModel.setLog("User " + email.getSender()
+                + " sending email: " + email.getId()
                 + " to: " + email.getReceivers().stream().collect(Collectors.joining(", ")));
 
         User sender = userService.readUserFromFileBlocking(email.getSender());
 
-        if(sender == null){
+        if (sender == null) {
             userService.unlock(email.getSender());
             //gestione sender non esistente: fatela voi a me è successo non so come
             logModel.setLog("ERRORE: utente non trovato");
-            out.writeObject(new Communication("emails_not_saved",new BooleanBody(sender.getUser(),false)));
+            out.writeObject(new Communication("emails_not_saved", new BooleanBody(sender.getUser(), false)));
             return;
         }
 
         sender.getEmails().add(email);
         userService.writeUserToFile(sender);
 
-        for (String receiver:email.getReceivers()) {
+        for (String receiver : email.getReceivers()) {
             User userReceiver = userService.readUserFromFileBlocking(receiver);
-            if(userReceiver == null){
+            if (userReceiver == null) {
                 userService.unlock(receiver);
-                logModel.setLog("ERRORE: "+ receiver + " non trovato");
+                logModel.setLog("ERRORE: " + receiver + " non trovato");
                 //gestione utente non esistente: fatela voi
                 continue;
-            }
-            else if(receiver.equals(email.getSender())) {
+            } else if (receiver.equals(email.getSender())) {
                 userService.unlock(receiver);
-                logModel.setLog("RECEIVER: "+ receiver + " uguale a sender: " + email.getSender());
+                logModel.setLog("RECEIVER: " + receiver + " uguale a sender: " + email.getSender());
                 continue;
             }
             userReceiver.getEmails().add(email);
             userService.writeUserToFile(userReceiver);
         }
 
-        out.writeObject(new Communication("emails_saved",new BooleanBody(sender.getUser(),true)));
+        out.writeObject(new Communication("emails_saved", new BooleanBody(sender.getUser(), true)));
     }
 
     private void getNewEmails(GetEmailsBody getEmailsBody) throws IOException {
@@ -110,42 +122,44 @@ public class ServerHandler implements Runnable{
         logModel.setLog("Get email by: " + getEmailsBody.getEmail());
 
         ArrayList<EmailBody> emailBodies = new ArrayList<>();
-        for(EmailBody e: user.getEmails()){
-            if(e.getTimestamp().after(getEmailsBody.getTimestamp()) && !e.getBin()) {
-              emailBodies.add(e);
+        for (EmailBody e : user.getEmails()) {
+            if (e.getTimestamp().after(getEmailsBody.getTimestamp()) && !e.getBin()) {
+                emailBodies.add(e);
             }
         }
 
-        out.writeObject(new Communication("get_emails_ok",new EmailListBody(getEmailsBody.getEmail(), emailBodies)));
+        out.writeObject(new Communication("get_emails_ok", new EmailListBody(getEmailsBody.getEmail(), emailBodies)));
     }
 
-    private void moveToBin(String id, String email) throws IOException{
+    private void moveToBin(String id, String email) throws IOException {
         User user = userService.readUserFromFileBlocking(email);
-        for(EmailBody e: user.getEmails()){
-            if(e.getId().equals(id)) {
+        for (EmailBody e : user.getEmails()) {
+            if (e.getId().equals(id)) {
                 e.setBin(true);
-                logModel.setLog("Client: "+ email +" moved to bin email with id " + id );
+                logModel.setLog("Client: " + email + " moved to bin email with id " + id);
             }
         }
         userService.writeUserToFile(user);
-        out.writeObject(new Communication("bin_ok",new BooleanBody(email, true)));
+        out.writeObject(new Communication("bin_ok", new BooleanBody(email, true)));
 
     }
 
     private void deletePermanently(String email) throws IOException {
         User user = userService.readUserFromFileBlocking(email);
-        int i = 0;;
+        int i = 0;
+        ;
 
-        if(user.getEmails().size() == 0 ){
+        if (user.getEmails().size() == 0) {
             out.writeObject(new Communication("delete_permanently_not_ok", new BooleanBody(email, false)));
         }
 
-        while(i < user.getEmails().size()){
-            if(user.getEmails().get(i).getBin()){
-                logModel.setLog("Client: "+ email +" remove email with id " + user.getEmails().get(i).getId());
+        while (i < user.getEmails().size()) {
+            if (user.getEmails().get(i).getBin()) {
+                logModel.setLog("Client: " + email + " remove email with id " + user.getEmails().get(i).getId());
                 user.getEmails().remove(i);
+            } else {
+                i++;
             }
-            else { i++;}
         }
         userService.writeUserToFile(user);
 
@@ -158,49 +172,49 @@ public class ServerHandler implements Runnable{
         try {
             try {
                 /* Stream sono un flusso di dati
-                * InputStream: sono i byte che ricevo in input
-                * OutputStream: sono i byte che invio in input
-                * */
-                InputStream inStream = socket.getInputStream();
-                in = new ObjectInputStream(inStream);
-                OutputStream outStream = socket.getOutputStream();
-                out= new ObjectOutputStream(outStream);
+                 * InputStream: sono i byte che ricevo in input
+                 * OutputStream: sono i byte che invio in input
+                 * */
+//                InputStream inStream = socket.getInputStream();
+//                in = new ObjectInputStream(inStream);
+//                OutputStream outStream = socket.getOutputStream();
+//                out= new ObjectOutputStream(outStream)
 
                 try {
-                        if(in == null || out == null) return;
+                    if (in == null || out == null) return;
 
-                        Communication communication = (Communication) in.readObject();
+                    Communication communication = (Communication) in.readObject();
 
-                        switch (communication.getAction()) {
-                            case "connection":
-                                connection(communication.getBody().getEmail());
-                                break;
-                            case "get_new_emails":
-                                getNewEmails((GetEmailsBody)communication.getBody());
-                                break;
-                            case "send_email":
-                                sendEmail((EmailBody) communication.getBody());
-                                break;
-                            case "bin":
-                                BinBody b = (BinBody) communication.getBody();
-                                moveToBin(b.getId(), b.getEmail());
-                                break;
-                            case "delete":
-                                deletePermanently(communication.getBody().getEmail());
-                                break;
-                        }
+                    switch (communication.getAction()) {
+                        case "connection":
+                            connection(communication.getBody().getEmail());
+                            break;
+                        case "get_new_emails":
+                            getNewEmails((GetEmailsBody) communication.getBody());
+                            break;
+                        case "send_email":
+                            sendEmail((EmailBody) communication.getBody());
+                            break;
+                        case "bin":
+                            BinBody b = (BinBody) communication.getBody();
+                            moveToBin(b.getId(), b.getEmail());
+                            break;
+                        case "delete":
+                            deletePermanently(communication.getBody().getEmail());
+                            break;
+                    }
                 } catch (ClassNotFoundException e) {
                     throw new RuntimeException(e);
                 }
 
-            }
-            finally {
+            } finally {
                 System.out.println("FINITO");
                 logModel.setLog("Client disconnected");
                 socket.close();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        catch (IOException e) {e.printStackTrace();}
     }
 
 }
